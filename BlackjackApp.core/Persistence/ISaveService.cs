3e720acf6016f7d1d28370b62f3aeb32ef90556f
dpngqd@umsystem.edu <dat.peter.nguyen1@gmail.com>
2026-09-14 11:40:40 -0500

    File cleaning

--- BlackjackApp.core/Persistence/ISaveService.cs
+++
@@ -1,8 +0,0 @@
-namespace BlackjackApp.core.Persistence;
-
-public interface ISaveService
-{
-    SaveData Load();
-
-    void Save(SaveData data);
-}
--- BlackjackApp.core/Persistence/JsonSaveService.cs
+++
@@ -1,8 +0,0 @@
-namespace BlackjackApp.core.Persistence;
-
-public class JsonSaveService : ISaveService
-{
-    public SaveData Load() => throw new NotImplementedException();
-
-    public void Save(SaveData data) => throw new NotImplementedException();
-}
